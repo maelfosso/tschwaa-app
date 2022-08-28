0b96cc32fd5487f45d886223ce6d3f86f0f94cd7
Mael FOSSO <fosso.mael.elvis@gmail.com>
2022-08-28 11:32:34 +0100

    added the sign-up page

--- pages/auth/sign-up.tsx
+++ pages/auth/sign-up.tsx
@@ -1,37 +1,79 @@
 import { LockClosedIcon } from '@heroicons/react/20/solid'
 import type { NextPage } from "next";
 
-const SignIn: NextPage = () => {
+const SignUp: NextPage = () => {
   return (
     <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div>
           <h2 className='mt-6 text-center text-3xl tracking-tight font-bold text-gray-900'>
-            Sign in to your account
+            Create an account
           </h2>
           <p className='mt-2 text-center text-sm text-gray-600'>
             Or{' '}
             <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-              create an account
+              sign into your an account
             </a>
-            {' '} if you don't have one
+            {' '} if you already have one
           </p>
         </div>
         <form className="mt-8 space-y-6" action="#" method="POST">
           <input type="hidden" name="remember" defaultValue="true" />
           <div className="rounded-md shadow-sm -space-y-px">
             <div>
-              <label htmlFor="username" className="sr-only">
-                Email / Phone
+              <label htmlFor="firstname" className="sr-only">
+                Firstname
               </label>
               <input
-                id="username"
-                name="username"
+                id="firstname"
+                name="firstname"
                 type="text"
-                autoComplete="email"
+                autoComplete="firstname"
                 required
                 className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
-                placeholder="Email address or Phone number"
+                placeholder="Firstname"
+              />
+            </div>
+            <div>
+              <label htmlFor="lastname" className="sr-only">
+                Lastname
+              </label>
+              <input
+                id="lastname"
+                name="lastname"
+                type="text"
+                autoComplete="lastname"
+                required
+                className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+                placeholder="Lastname"
+              />
+            </div>
+            <div>
+              <label htmlFor="email" className="sr-only">
+                Email
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+                placeholder="Email address"
+              />
+            </div>
+            <div>
+              <label htmlFor="username" className="sr-only">
+                Phone
+              </label>
+              <input
+                id="phone"
+                name="phone"
+                type="phone"
+                autoComplete="phone"
+                required
+                className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+                placeholder="Phone number"
               />
             </div>
             <div>
@@ -50,35 +92,12 @@ const SignIn: NextPage = () => {
             </div>
           </div>
 
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <input
-                id="remember-me"
-                name="remember-me"
-                type="checkbox"
-                className='h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded'
-              />
-              <label htmlFor="remember-me" className='ml-2 block text-sm text-gray-900'>
-                Remember me
-              </label>
-            </div>
-
-            <div className='text-sm'>
-              <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                Forgot your password?
-              </a>
-            </div>
-          </div>
-
           <div>
             <button
               type="submit"
               className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
             >
-              <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                <LockClosedIcon className='h-5 w-5 text-indigo-500 group-hover:text-indigo-400' aria-hidden="true"/>
-              </span>
-              Sign in
+              Sign up
             </button>
           </div>
         </form>
@@ -87,4 +106,4 @@ const SignIn: NextPage = () => {
   );
 }
 
-export default SignIn;
+export default SignUp;
