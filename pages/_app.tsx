3b655eb3c8a9adb5e715710e0eaded86c5794175
Mael FOSSO <fosso.mael.elvis@gmail.com>
2022-09-03 15:22:20 +0100

    added a protected component for protected route

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,17 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router';
 import Layout from '../components/Layout';
+import { SessionProvider } from 'next-auth/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   return (
-    <Layout>
-      <Component key={router.asPath} {...pageProps} />
-    </Layout>
+    <SessionProvider session={pageProps.session}>
+      <Layout>
+        <Component key={router.asPath} {...pageProps} />
+      </Layout>
+    </SessionProvider>
   );
 }
 
