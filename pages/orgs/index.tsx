4ee7f402ce81990030f589e278586b42edf0475f
Mael FOSSO <fosso.mael.elvis@gmail.com>
2022-09-27 10:19:41 +0100

    added form for creating an organization

--- pages/orgs/index.tsx
+++ pages/orgs/index.tsx
@@ -1,9 +1,9 @@
-import { Menu, Transition } from "@headlessui/react";
-import { BriefcaseIcon, CalendarIcon, CheckIcon, ChevronDownIcon, CurrencyDollarIcon, LinkIcon, MapPinIcon, PencilIcon, PlusIcon } from "@heroicons/react/20/solid";
+import { Dialog, Menu, Transition } from "@headlessui/react";
+import { BriefcaseIcon, CalendarIcon, CheckIcon, ChevronDownIcon, CurrencyDollarIcon, LinkIcon, MapPinIcon, PencilIcon, PlusIcon, XMarkIcon } from "@heroicons/react/20/solid";
 import { unstable_getServerSession } from "next-auth";
 import { getSession, useSession } from "next-auth/react";
 import Router from "next/router";
-import { Fragment, useEffect } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { get } from "../../utils/axios";
 import { classNames } from "../../utils/utils";
 import { authOptions } from "../api/auth/[...nextauth]"
@@ -20,12 +20,17 @@ interface OrgsProps {
 
 const Orgs = ({ orgs }: OrgsProps ) => {
   const { data, status } = useSession();
+  const [open, setOpen] = useState<boolean>(false)
   
   useEffect(() => {
     if (status === "unauthenticated") {
       Router.replace("/auth/sign-in")
     }
   });
+
+  const onNewOrganization = () => {
+    setOpen(true);
+  }
 
   return (
     <div className="container mx-auto px-4 sm:px-6 lg:px-8">
@@ -39,6 +44,7 @@ const Orgs = ({ orgs }: OrgsProps ) => {
           <button
             type="button"
             className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            onClick={() => onNewOrganization()}
           >
             <PlusIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
             New Organization
@@ -70,6 +76,7 @@ const Orgs = ({ orgs }: OrgsProps ) => {
             <button
               type="button"
               className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              onClick={() => onNewOrganization()}
             >
               <PlusIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
               New Organization
@@ -77,6 +84,113 @@ const Orgs = ({ orgs }: OrgsProps ) => {
           </div>
         </div>
       </div>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={setOpen}>
+          <div className="fixed inset-0" />
+
+          <div className="fixed inset-0 overflow-hidden">
+            <div className="absolute inset-0 overflow-hidden">
+              <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16">
+                <Transition.Child
+                  as={Fragment}
+                  enter="transform transition ease-in-out duration-500 sm:duration-700"
+                  enterFrom="translate-x-full"
+                  enterTo="translate-x-0"
+                  leave="transform transition ease-in-out duration-500 sm:duration-700"
+                  leaveFrom="translate-x-0"
+                  leaveTo="translate-x-full"
+                >
+                  <Dialog.Panel className="pointer-events-auto w-screen max-w-2xl">
+                    <form className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                      <div className="flex-1">
+                        {/* Header */}
+                        <div className="bg-gray-50 px-4 py-6 sm:px-6">
+                          <div className="flex items-start justify-between space-x-3">
+                            <div className="space-y-1">
+                              <Dialog.Title className="text-lg font-medium text-gray-900">New Organizations</Dialog.Title>
+                              <p className="text-sm text-gray-500">
+                                Get Started by filling in the information below to create your new organization
+                              </p>
+                            </div>
+                            <div className="flex h-7 items-center">
+                              <button>
+                                <span className="sr-only">Close panel</span>
+                                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+
+                        {/* Divider container */}
+                        <div className="space-y-6 py-6 sm:space-y-0 sm:divide-y sm:divide-gray-200 sm:py-0">
+                          {/* Organization name */}
+                          <div className="space-y-1 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
+                            <div>
+                              <label
+                                htmlFor="organization-name"
+                                className="block text-sm font-medium text-gray-900 sm:mt-px sm:pt-2"
+                              >
+                                Organization name
+                              </label>
+                            </div>
+                            <div className="sm:col-span-2">
+                              <input
+                                type="text"
+                                name="organization-name"
+                                id="organization-name"
+                                className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                              />
+                            </div>
+                          </div>
+
+                          {/* Organization description */}
+                          <div className="space-y-1 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
+                            <div>
+                              <label
+                                htmlFor="organization-description"
+                                className="block text-sm font-medium text-gray-900 sm:mt-px sm:pt-2"
+                              >
+                                Organization description
+                              </label>
+                            </div>
+                            <div className="sm:col-span-2">
+                              <textarea
+                                name="organization-description"
+                                id="organization-description"
+                                rows={3}
+                                className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                defaultValue={''}
+                              />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      {/* Action buttons */}
+                      <div className="flex-shrink-0 border-t border-gray-200 px-4 py-5 sm:px-6">
+                        <div className="flex justify-end space-x-3">
+                          <button
+                            type="button"
+                            className="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            onClick={() => setOpen(false)}
+                          >
+                            Cancel
+                          </button>
+                          <button
+                            type="button"
+                            className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                          >
+                            Create
+                          </button>
+                        </div>
+                      </div>
+                    </form>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
     </div>
   )
 }
