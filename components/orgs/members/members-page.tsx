1d17e65174929729ba664d9feeaf4ec00dd61684
Mael FOSSO <fosso.mael.elvis@gmail.com>
2023-08-21 09:32:28 +0100

    added a menu to member row for edit and delete buttons

--- components/orgs/members/members-page.tsx
+++ components/orgs/members/members-page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Dialog, Menu, Transition } from "@headlessui/react";
-import { PaperAirplaneIcon, PlusIcon, UserPlusIcon, XMarkIcon } from "@heroicons/react/20/solid";
+import { EllipsisVerticalIcon, PaperAirplaneIcon, PencilSquareIcon, PlusIcon, TrashIcon, UserPlusIcon, XMarkIcon } from "@heroicons/react/20/solid";
 import { Fragment, useEffect, useState } from "react";
 import { Notification } from "../../notification";
 import { sendInviteOnWhatsapp, sendMultipleWhatsappInvitation } from "../../../services/organizations";
@@ -382,9 +382,76 @@ const MembersPage = ({ organizationId, members }: MembersPageProps) => {
                       }
                     </td>
                     <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6 lg:pr-8">
-                      <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                      {/* <a href="#" className="text-indigo-600 hover:text-indigo-900">
                         Edit (replaced by dropdown menu with actions)<span className="sr-only">{`${member.firstName} ${member.lastName}`}</span>
-                      </a>
+                      </a> */}
+                      <Menu as="div" className="relative inline-block text-left">
+                        <div>
+                          <Menu.Button className="flex items-center rounded-full bg-gray-100 text-gray-400 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+                            <span className="sr-only">Open options</span>
+                            <EllipsisVerticalIcon className="h-5 w-5" aria-hidden="true" />
+                          </Menu.Button>
+                        </div>
+
+                        <Transition
+                          as={Fragment}
+                          enter="transition ease-out duration-100"
+                          enterFrom="transform opacity-0 scale-95"
+                          enterTo="transform opacity-100 scale-100"
+                          leave="transition ease-in duration-75"
+                          leaveFrom="transform opacity-100 scale-100"
+                          leaveTo="transform opacity-0 scale-95"
+                        >
+                          <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <div className="py-1">
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <a
+                                    href="#"
+                                    className="'text-gray-700 group flex items-center px-4 py-2 text-sm"
+                                  >
+                                    <PencilSquareIcon
+                                      className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                                      aria-hidden="true"
+                                    />
+                                    Edit
+                                  </a>
+                                )}
+                              </Menu.Item>
+                              {/* <Menu.Item>
+                                {({ active }) => (
+                                  <a
+                                    href="#"
+                                    className={classNames(
+                                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                      'group flex items-center px-4 py-2 text-sm'
+                                    )}
+                                  >
+                                    <DocumentDuplicateIcon
+                                      className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                                      aria-hidden="true"
+                                    />
+                                    Duplicate
+                                  </a>
+                                )}
+                              </Menu.Item> */}
+                            </div>
+                            <div className="py-1">
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <a
+                                    href="#"
+                                    className="text-gray-700 group flex items-center px-4 py-2 text-sm"
+                                  >
+                                    <TrashIcon className="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500" aria-hidden="true" />
+                                    Delete
+                                  </a>
+                                )}
+                              </Menu.Item>
+                            </div>
+                          </Menu.Items>
+                        </Transition>
+                      </Menu>
                     </td>
                   </tr>
                 ))}
